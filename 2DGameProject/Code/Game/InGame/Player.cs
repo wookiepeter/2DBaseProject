68af376f29499500dafef14a2dc4612ea87a512d
DarkSl4yer <dzhu95@yandex.ru>
2016-06-24 22:11:21 +0200

    http://whatthecommit.com/

--- 2DGameProject/Code/Game/InGame/Player.cs
+++ 2DGameProject/Code/Game/InGame/Player.cs
@@ -11,35 +11,44 @@ namespace GameProject2D
 {
     public class Player
     {
+        public bool isJumping;
+
         RectangleShape sprite;
         Vector2f position { get { return sprite.Position; } set { sprite.Position = value; } }
         Vector2f movement { get; set; }
         Vector2f size { get { return sprite.Size; } set { sprite.Size = value; } }
+        Vector2f gravity = new Vector2f(0F, 2F);
 
 
         public Player(Vector2f position)
         {
             this.sprite = new RectangleShape(new Vector2f(1F, 1F));
-            this.sprite.FillColor = Color.Black;
+            this.sprite.FillColor = Color.Red;
 
             this.position = position;
             this.movement = new Vector2f(0F, 0F);
             
-            this.size = new Vector2f(100F, 100F);
+            this.size = new Vector2f(45F, 45F);
         }
 
         public void update(float deltaTime)
         {
-            float speed = deltaTime;
+            Console.WriteLine(deltaTime);
+            float speed = 0.005F;
             
             Vector2f inputMovement = new Vector2f(0F, 0F);
 
             inputMovement.Y += Keyboard.IsKeyPressed(Keyboard.Key.Down) ? speed : 0F;
-            inputMovement.Y += Keyboard.IsKeyPressed(Keyboard.Key.Up) ? -speed : 0F;
+           // inputMovement.Y += Keyboard.IsKeyPressed(Keyboard.Key.Up) ? -speed : 0F;
 
             inputMovement.X += Keyboard.IsKeyPressed(Keyboard.Key.Left) ? -speed : 0F;
             inputMovement.X += Keyboard.IsKeyPressed(Keyboard.Key.Right) ? speed : 0F;
 
+            if (KeyboardInputManager.IsPressed(Keyboard.Key.Up))
+            {
+                inputMovement.Y -= 100F; //wie hoh der fliegt
+                isJumping = true;
+            }
             if(inputMovement.Y != 0F || inputMovement.X != 0F)
             {
                 movement += inputMovement * speed / (float)Math.Sqrt(inputMovement.X * inputMovement.X + inputMovement.Y * inputMovement.Y);
@@ -49,10 +58,28 @@ namespace GameProject2D
 
             position += movement;
 
-            if(position.X < 0)
+           if(position.X < 0)
+            {
+                position -= movement;
+                movement *= Vector2.Left;
+            }
+
+            if (position.Y < 0)
             {
                 position -= movement;
                 movement *= Vector2.Up;
+            }
+
+            if (position.Y > Program.win.Size.Y - sprite.Size.Y) // Windowsize - Playersize
+            {
+                position -= movement;
+                movement *= Vector2.Right;
+            }
+
+            if (position.X > Program.win.Size.X - sprite.Size.X)
+            {
+                position -= movement;
+                movement *= Vector2.Left;
             }
         }
 
