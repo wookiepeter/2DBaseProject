a58482724eff0bc61e3a7b0ee868d72f2096dcc0
DarkSl4yer <dzhu95@yandex.ru>
2016-06-25 12:46:45 +0200

    I'm too old for this shit!

---
+++ 2DGameProject/Code/Game/InGame/Plant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace GameProject2D
+{
+     class Plant
+    {
+        
+        Sprite sprite;
+        float SpriteWidth { get { return sprite.Texture.Size.X * sprite.Scale.X; } }
+        float SpriteHeigh { get { return sprite.Texture.Size.Y * sprite.Scale.Y; } }
+        public Plant()
+        {
+            AssetManager.GetTexture(AssetManager.TextureName.Crop); //greift auf die Texture zu
+            sprite = new Sprite(AssetManager.GetTexture(AssetManager.TextureName.Crop));
+            this.sprite.Position = new Vector2f(50F, (Program.win.Size.Y * 0.8F)- SpriteHeigh);
+        }
+
+    
+
+        public void Draw(RenderWindow win, View view)
+        {
+            win.Draw(sprite);
+        }
+
+        public void DrawGUI(GUI gui, float deltaTime)
+        {
+        }
+    }
+}
