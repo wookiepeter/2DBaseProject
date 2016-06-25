12cabc7d2cb77d1cc77b61c5a898004d3abf7f83
DarkSl4yer <dzhu95@yandex.ru>
2016-06-25 02:44:36 +0200

    Feed. You. Stuff. No time.

---
+++ 2DGameProject/Code/Game/InGame/Background.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace GameProject2D
+{
+    class Background
+    {
+        RectangleShape island;
+        RectangleShape island2;
+
+        public Background()
+        {
+            this.island = new RectangleShape(new Vector2f(300F, 45F));
+            this.island.Position = new Vector2f(0F, Program.win.Size.Y * 0.8F);
+            this.island.FillColor = Color.Black;
+            this.island2 = new RectangleShape(new Vector2f(300F, 45F));
+            this.island2.Position = new Vector2f(500F, Program.win.Size.Y * 0.8F);
+            this.island2.FillColor = Color.Black;
+        }
+
+    
+
+        public void Draw(RenderWindow win, View view)
+        {
+            win.Draw(island);
+            win.Draw(island2);
+        }
+
+        public void DrawGUI(GUI gui, float deltaTime)
+        {
+        }
+    }
+}
